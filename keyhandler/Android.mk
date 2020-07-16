390f86d59672216f4f607110226cb1c94e2da272
Secretwolf98 <jordanluders@gmail.com>
2020-07-16 07:05:17 -0700

    Fixes.

--- keyhandler/Android.mk
+++ keyhandler/Android.mk
@@ -1,3 +1,5 @@
+ifeq ($(filter n8000 n8000_deodexed n8010 n8013 n8020 i925,$(TARGET_DEVICE)),)
+
 LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
@@ -6,3 +8,5 @@ LOCAL_SRC_FILES := $(call all-java-files-under,src)
 LOCAL_MODULE_TAGS := optional
 LOCAL_DEX_PREOPT := false
 include $(BUILD_JAVA_LIBRARY)
+
+endif
